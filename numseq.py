3dd838c68f5390b15585a98a8f9b3bd68a5439f7
Eileen <eileen.tallman@gmail.com>
2019-07-05 22:17:28 -0400

    Added docstrings

--- numseq.py
+++ numseq.py
@@ -1,26 +1,28 @@
 #Fibonacci
+'''Within the numseq package, creates a module named fib. Within the fib module, defines a function fib(n) that returns the nth Fibonacci number.'''
 from numseq.fib import fib
-
-print ("Fibonacci")
+    print ("Fibonacci")
 for i in range(10):
     print ("{}: {}".format(i, fib(i)))
-    
+
     
 #Geo
-from numseq.geo import *
+    '''Within the numseq package, create a module named geo. Within the geo module, define 3 functions: square(n), triangle(n), and cube(n)'''
+    from numseq.geo import *
 
-print("Geometric numbers (square, triangle, cube)")
-for i in range(10):
+    print("Geometric numbers (square, triangle, cube)")
+    for i in range(10):
     print ("{}: {} {} {}".format(i, square(i), triangle(i), cube(i)))
 
     
 #Primes
-from numseq.prime import *
+    '''Creates a module named prime within the numseq package. Defines the following functions: 1) primes(n) returns a list of all primes less than n and 2)
+    is_prime(m) returns a boolean indicating whether m is a prime number.'''
+    from numseq.prime import *
 
-print ("Primes")
-prime_list = primes(1000)
-for p in prime_list[-10:]:
-    print (p)
+    print ("Primes")
+    prime_list = primes(1000)
+    for p in prime_list[-10:]:
+        print (p)
 print ("Is 999981 prime? {}".format(is_prime(999981)))
 print ("Is 999983 prime? {}".format(is_prime(999983)))
-
