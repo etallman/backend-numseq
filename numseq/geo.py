f31ee7e6d485f9c6517a149c69a587e28ebdccdd
Eileen <eileen.tallman@gmail.com>
2019-07-12 14:41:20 -0400

    Completed

--- numseq/geo.py
+++ numseq/geo.py
@@ -4,13 +4,15 @@ def square(n):
     for i in range(0, n):
         return n * n
 
-
+#After being stuck, I moved onto the next lesson and found the solution.
 def triangle(n):
-        # result=0
-        # for i in range(1, n):
         
-        # return result
-        pass
+        result=0
+        for i in range(1, n):
+                result = i * (i+1) / 2
+                
+        return result
+       
 
 
 def cube(n):
