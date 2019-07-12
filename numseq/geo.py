ea3a28b2c050cc41b89f7f58885c79b2875b51c5
Eileen <eileen.tallman@gmail.com>
2019-07-12 14:13:14 -0400

    Stuck on triangle

--- numseq/geo.py
+++ numseq/geo.py
@@ -2,21 +2,17 @@
 '''Within the numseq package, create a module named geo. Within the geo module, define 3 functions: square(n), triangle(n), and cube(n)'''
 def square(n):
     for i in range(0, n):
-        return i * i
+        return n * n
 
 
 def triangle(n):
-        result=0
-        for i in range(1, n):
-                if i > 1:
-                        next_i = i + 1
-                        result = next_i + (i + 1)
-                else:
-                        next_i = i + 1
-                        result = i + next_i
-        return result
+        # result=0
+        # for i in range(1, n):
+        
+        # return result
+        pass
 
 
 def cube(n):
     for i in range(0, n):
-        return i * i * i
+        return n * n * n
